c9973dd58267a3c1ed5ea7de52b6fa3a5275bf47
ghasto69 <tiny.ghasto@gmail.com>
2025-02-14 13:18:43 +0400

    small fixes

--- src/main/java/overcooked_orange/frogged/registry/ModBlocks.java
+++ src/main/java/overcooked_orange/frogged/registry/ModBlocks.java
@@ -1,16 +1,19 @@
 package overcooked_orange.frogged.registry;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
 import overcooked_orange.frogged.Frogged;
-import overcooked_orange.frogged.registry.ModFrogs;
 
 import java.util.function.BiFunction;
 import java.util.function.Function;
@@ -29,17 +32,18 @@ public class ModBlocks {
     );
 
     public static void registerBlocks() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
-            entries.addAfter(Items.PEARLESCENT_FROGLIGHT, ModBlocks.OBSIDIAN_FROGLIGHT);
-        });
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
-            entries.addAfter(Items.PEARLESCENT_FROGLIGHT, ModBlocks.OBSIDIAN_FROGLIGHT);
-        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(ModBlocks::addFroglight);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(ModBlocks::addFroglight);
     }
 
     private static Block block(String name, Function<AbstractBlock.Settings, Block> blockFunction, AbstractBlock.Settings blockSettings, BiFunction<Block, Item.Settings, Item> itemFunction, Item.Settings itemSettings) {
-        Block block = Blocks.register(RegistryKey.of(RegistryKeys.BLOCK, Frogged.id(name)), blockFunction, blockSettings);
-        Items.register(block, itemFunction, itemSettings);
+        Identifier id = Frogged.id(name);
+        Block block = Registry.register(Registries.BLOCK, id, blockFunction.apply(blockSettings.registryKey(RegistryKey.of(RegistryKeys.BLOCK, id))));
+        Registry.register(Registries.ITEM, id, itemFunction.apply(block, itemSettings.registryKey(RegistryKey.of(RegistryKeys.ITEM, id))));
         return block;
+    }
+
+    private static void addFroglight(FabricItemGroupEntries entries) {
+        entries.addAfter(Items.PEARLESCENT_FROGLIGHT, ModBlocks.OBSIDIAN_FROGLIGHT);
     }
 }
